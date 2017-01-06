cf26fbb40e9cee0dadad1bff6062edd3301a25b4
zhoujian <1064791247@qq.com>
2017-01-06 15:42:24 +0800

    zeke  提交代码

--- app/src/main/java/com/zhoujian/picasso/activity/MainActivity.java
+++ app/src/main/java/com/zhoujian/picasso/activity/MainActivity.java
@@ -27,13 +27,16 @@ public class MainActivity extends Activity
         initDates();
         setDatas();
     }
+
     private void setDatas()
     {
         PictureAdapter adapter = new PictureAdapter(MainActivity.this,personList);
         mListPerson.setAdapter(adapter);
     }
+
     private void initDates()
     {
+
         Person person0 = new Person("http://pic47.nipic.com/20140830/7487939_180041822000_2.jpg","湖光山色");
         personList.add(person0);
         Person person1 = new Person("http://pic41.nipic.com/20140518/4135003_102912523000_2.jpg","万紫千红");
@@ -46,11 +49,11 @@ public class MainActivity extends Activity
         personList.add(person4);
         Person person5 = new Person("http://img2.3lian.com/2014/c7/51/d/26.jpg","春山如笑");
         personList.add(person5);
+
     }
 
     @Override
-    protected void onDestroy()
-    {
+    protected void onDestroy() {
         super.onDestroy();
         mBind.unbind();
     }
--- app/src/main/java/com/zhoujian/picasso/adapter/PictureAdapter.java
+++ app/src/main/java/com/zhoujian/picasso/adapter/PictureAdapter.java
@@ -1,15 +1,21 @@
 package com.zhoujian.picasso.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 import com.zhoujian.picasso.R;
 import com.zhoujian.picasso.bean.Person;
+import com.zhoujian.picasso.transform.RoundedTransformationBuilder;
+
 import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -62,8 +68,18 @@ public class PictureAdapter extends BaseAdapter
         }
 
         if (persons != null && persons.size() > 0) {
+
+
+            Transformation transformation = new RoundedTransformationBuilder()
+                    .borderColor(Color.TRANSPARENT)
+                    .borderWidthDp(0)
+                    .cornerRadiusDp(20)
+                    .oval(false)
+                    .build();
+
             Picasso.with(context)
                     .load(persons.get(position).getPictureUrl())
+                    .transform(transformation)
                     .placeholder(R.mipmap.image_default)//没有加载图片时显示的默认图像
                     .error(R.mipmap.image_default)// 图像加载错误时显示的图像
                     .into(holder.mImg);// 被加载的控件
